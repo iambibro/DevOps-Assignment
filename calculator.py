0ddf1fed95be6bc682c7f065869f1ffec45b5a88
Bibrata Barman <33283305+iambibro@users.noreply.github.com>
2023-03-27 15:19:39 +0530

    commit 1

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 def add(a, b):
     if a < 0 or b < 0:
-        raise ValueError("Both numbers should be positive")
+        raise ValueError("Both numbers should be positive ")
     return a + b
 
 def subtract(a, b):
